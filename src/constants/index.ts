2f55ee5f0e50a386b6292ceaee3ab678395394be
Đồng Minh Dương <dongminhduong991@gmail.com>
2023-11-19 23:20:02 +0700

    update folder constants, helpers, utils

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+const RoleShop = {
+  WRITER: '001',
+  EDITED: '002',
+  ADMIN: '003',
+  SHOP: '004',
+};
+
+const HEADER = {
+  API_KEY: 'x-api-key',
+  AUTHORIZATION: 'authorization',
+};
+
+export { RoleShop, HEADER };
